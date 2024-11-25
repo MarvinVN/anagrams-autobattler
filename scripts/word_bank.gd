943366eb110caa41f67c74e622015eebd59d0500
MarvinVN <marvinvnguyen@gmail.com>
2024-11-25 18:28:06 -0500

    get letters + valid words on start

--- scripts/word_bank.gd
+++ scripts/word_bank.gd
@@ -1,13 +1,28 @@
 class_name WordBank
 extends Node
 
-@export var words: Dictionary = {}
-var file: FileAccess
+var words: Dictionary = {}
+var words_len6: Dictionary = {}
 
-func _init():
+func _ready() -> void:
+	init_word_bank()
+	init_word_bank_len6()
+
+func get_random_word() -> String:
+	return words_len6.keys().pick_random()
+
+func init_word_bank():
 	var file = FileAccess.open("res://resources/word_bank.txt", FileAccess.READ)
 	while file.eof_reached() == false:
 		var word = file.get_line().strip_edges()
 		if word != "":
 			words[word] = true
+	file.close()
+
+func init_word_bank_len6():
+	var file = FileAccess.open("res://resources/word_bank_length_6.txt", FileAccess.READ)
+	while file.eof_reached() == false:
+		var word = file.get_line().strip_edges()
+		if word != "":
+			words_len6[word] = true
 	file.close()
---
+++ scripts/word_manager.gd
@@ -0,0 +1,34 @@
+class_name WordManager
+extends Node
+
+@export var word_bank: WordBank
+var letter_set: Array
+var valid_words: Dictionary
+
+func _ready() -> void:
+	new_letter_set()
+
+func new_letter_set() -> void:
+	var word = word_bank.get_random_word()
+	var letters = []
+	for char in word:
+		letters.append(char)
+	letter_set = letters
+	generate_valid_words()
+	
+func generate_valid_words() -> void:
+	var valid_word_set = {}
+	var letters = letter_set
+	for x in range(1, letter_set.size() + 1):
+		generate_words("", letters, 0, valid_word_set)
+	valid_words = valid_word_set
+	
+func generate_words(word: String, remaining_letters: Array, current_idx: int, valid_word_set: Dictionary) -> void:
+	if current_idx == len(word):
+		if word_bank.words.has(word):
+			valid_word_set[word] = true
+			
+	for i in range(remaining_letters.size()):
+		var next_word = word + remaining_letters[i]
+		var next_remaining = remaining_letters.slice(0, i) + remaining_letters.slice(i + 1, remaining_letters.size())
+		generate_words(next_word, next_remaining, current_idx+1, valid_word_set)
