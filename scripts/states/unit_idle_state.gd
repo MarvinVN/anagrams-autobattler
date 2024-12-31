ece7a08532c5d5b02d7682f713a2961479e3b143
MarvinVN <marvinvnguyen@gmail.com>
2024-12-30 23:03:15 -0500

    pursuit state

--- scripts/states/unit_idle_state.gd
+++ scripts/states/unit_idle_state.gd
@@ -13,6 +13,12 @@ func enter() -> void:
 	unit.animated_sprite.play("blue_pawn_idle")
 	timer.start(1)
 
+func update(delta: float) -> void:
+	if unit.current_target:
+		state_transition.emit(self, "Pursuit")
+	if timer.is_stopped():
+		timer.start(1)
+
 func start_march() -> void:
 	unit.pathfind_component.set_target_position(unit.team_component.enemy_base_pos)
 	timer.stop()
--- scripts/states/unit_march_state.gd
+++ scripts/states/unit_march_state.gd
@@ -4,6 +4,10 @@ extends UnitState
 func enter() -> void:
 	unit.animated_sprite.play("blue_pawn_walk")
 
+func update(delta: float) -> void:
+	if unit.current_target:
+		state_transition.emit(self, "Pursuit")
+
 func physics_update(delta: float) -> void:
 	if NavigationServer2D.map_get_iteration_id(unit.pathfind_component.nav_agent.get_navigation_map()) == 0:
 		return
