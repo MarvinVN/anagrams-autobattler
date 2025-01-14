c90cd07ec95430c2f122dd2f0f812edc123da67e
MarvinVN <marvinvnguyen@gmail.com>
2025-01-13 23:23:25 -0500

    remove unit from unit manager on death

--- scripts/autobattler/unit_manager.gd
+++ scripts/autobattler/unit_manager.gd
@@ -17,12 +17,12 @@ func spawn_unit(word_length: int, team: int) -> Unit:
 		6: pass
 	add_child(new_unit)
 	new_unit.team_component.team = team
+	new_unit.health_component.health_below_zero.connect(_on_unit_death.bind(new_unit))
 	if team == 2:
 		new_unit.flip_direction()
 	new_unit.position = baseA.get_random_spawn_point() if team == 1 else baseB.get_random_spawn_point()
 	unit_list.append(new_unit)
 	return new_unit
 
-func destroy_unit(unit: Unit) -> void:
+func _on_unit_death(unit: Unit) -> void:
 	unit_list.erase(unit)
-	unit.queue_free()
